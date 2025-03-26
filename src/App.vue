de428e95883f67c6f242cbe87562a225c697c82c
Ethan-shen-Official <13382012505@163.com>
2025-03-26 21:01:06 +0800

    'update'

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
   <el-container type="vertical">
-  <router-view />
+    <router-view />
   </el-container>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import {router} from './router'
+import {routes} from './router'
 import App from './App.vue'
 import axios from 'axios'
 import ElementPlus from 'element-plus'
@@ -10,4 +10,4 @@ import './style.css'
 axios.defaults.baseURL = ("http://localhost:8080")
 axios.defaults.timeout = 30000;
 
-createApp(App).use(ElementPlus).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(routes).mount('#app')
