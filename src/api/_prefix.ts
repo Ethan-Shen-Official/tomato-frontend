89552ae761858c2fe3ee8dd1df0874eab199ac88
Ethan-shen-Official <13382012505@163.com>
2025-03-26 22:51:02 +0800

    %npm_config_msg%

--- src/api/_prefix.ts
+++ src/api/_prefix.ts
@@ -3,4 +3,5 @@ export const API_MODULE_PREFIX = '/api'
 //user_module
 export const API_USER_MODULE_PREFIX = API_MODULE_PREFIX + 'accounts'
 
-//
+//tools_module
+export const API_TOOLS_MODULE_PREFIX = API_MODULE_PREFIX + 'tools'
---
+++ src/api/tools.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { API_TOOLS_MODULE_PREFIX } from './_prefix';
+
+export uploadimg = (payload: any) => {
+    //TODO POST request
+}
