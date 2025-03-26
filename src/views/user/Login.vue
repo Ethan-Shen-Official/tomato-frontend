a0d04ee52cb6bd2a07cbf99a25aad46fabf34803
Ethan-shen-Official <13382012505@163.com>
2025-03-26 22:15:34 +0800

    'update'

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -3,28 +3,9 @@
 </script>
 
 <template>
-    <div class="flex justify-center items-center h-screen">
-        <div class="big-circle"></div>
-    </div>
+
 </template>
 
 <style scoped>
-.big-circle {
-    width: 300px;
-    height: 300px;
-    background-color: #3b82f6;
-    border-radius: 50%;
-}
-.flex {
-    display: flex;
-}
-.justify-center {
-    justify-content: center;
-}
-.items-center {
-    align-items: center;
-}
-.h-screen {
-    height: 100vh;
-}
+
 </style>
