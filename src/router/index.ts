289ec21a13a5dd7fc3a7737528c5ab1e57f3bcd1
Ethan-shen-Official <13382012505@163.com>
2025-03-26 20:34:59 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,5 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [{
+      path: '/',
+      redirect: '/login',
+  },
+  {
+    path: '/login',
+    component: () => import('../views/Login.vue'),
+    meta: { title : 'Login' }
+  },
+  {
+    path: '/register',
+    component: () => import('../views/Register.vue'),
+    meta: { title : 'Register' }
+  }
+  ],
 })
