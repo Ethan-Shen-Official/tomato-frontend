9113f2b2ac3cf37427367d416ed3f418be18d328
Ethan-shen-Official <13382012505@163.com>
2025-03-27 17:56:09 +0800

    auto-update

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -11,4 +11,28 @@ service.interceptors.request.use(
   }
 );
 
+service.interceptors.request.use(config => {
+    const token = sessionStorage.getItem('token')
+    if (token) {
+        config.headers.token = token
+    }
+    return config
+},error => {
+    return Promise.reject(error)
+})
+
+//对后端请求校验HTTP状态码
+service.interceptors.response.use(
+  (response) => {
+    if (response.status === 200) {
+      return response;
+    } else {
+      return Promise.reject(response);
+    }
+  }, (error) => {
+    console.log(error)
+    return Promise.reject(error);
+  }
+)
+
 export {service as axios};
