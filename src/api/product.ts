7ded73f4dab9ce56b4496d12e9dda5b28dd609ab
Ethan-shen-Official <13382012505@163.com>
2025-04-02 19:29:34 +0800

    auto-update

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,16 +1,39 @@
 import { API_PRODUCT_MODULE_PREFIX } from "./_prefix";
 import { axios } from "../utils/request";
 
-type ProductInfo = {
+type UpdateInfo = {
     id: string;
-    title: string;
-    //price: BigDecimal;
-    //rate: Double;
-    //description?
-    //cover?
-    //detail?
-    //specifications?   
+    title?: string;
+    price?: string;
+    rate?: number;
+    description?: string;
+    cover?: string;
+    detail?: string;
+    specifications?: specification[];
 }
+
+type AddInfo = {
+    title: string;
+    price: string;
+    rate: number;
+    description?: string;
+    cover?: string;
+    detail?: string;
+    specifications?: specification[];
+}
+
+type specification = {
+    id: string;
+    item: string;
+    value: string;
+    productId: string;
+}
+
+type productAmount = {
+    id: string;
+    amount: number;
+}
+
 
 /**
  * 请求方式
@@ -20,8 +43,7 @@ type ProductInfo = {
  * @returns products
  */
 export const getProducts = () => {
-    return axios.get(`${API_PRODUCT_MODULE_PREFIX}`,
-    )
+    return axios.get(`${API_PRODUCT_MODULE_PREFIX}`)
     .then(res => {
         return res
     })
@@ -34,7 +56,7 @@ export const getProducts = () => {
     config: none
     params: id: string
  * @param id
- * @returns product
+ * @returns productinfo[]
  */
 export const getProduct = (id: String) => {
     return axios.get(`${API_PRODUCT_MODULE_PREFIX}/${id}`,
@@ -46,15 +68,92 @@ export const getProduct = (id: String) => {
 
 /**
  * 请求方式
+    方法: GET
+    路径: /api/products/{id}
+    config: none
+ * @param id 
+ * @returns productinfo
+ */
+export const getProductById = (id: string) => {
+    return axios.get(`${API_PRODUCT_MODULE_PREFIX}/${id}`)
+    .then(res => {
+        return res
+    })
+}
+
+/**
+ * 请求方式
     方法: PUT
     路径: /api/products
     config: application/json
- * @param productinfo 
+ * @param UpdateInfo 
  * @returns update result
  */
-export const updateProduct = (productinfo : ProductInfo) => {
+export const updateProduct = (productinfo : UpdateInfo) => {
     return axios.put(`${API_PRODUCT_MODULE_PREFIX}`,productinfo,
         {headers: { 'Content-Type': 'application/json' }})
+    .then(res => {
+        return res
+    })
+}
+
+/**
+ * 请求方式
+    方法: POST
+    路径: /api/products
+    config: application/json
+    * @param AddInfo
+    * @returns add result
+*/
+export const addProduct = (productinfo : AddInfo) => {
+    return axios.post(`${API_PRODUCT_MODULE_PREFIX}`,productinfo,
+        {headers: { 'Content-Type': 'application/json' }})
+    .then(res => {
+        return res
+    })
+}
+
+/**
+ * 请求方式
+    方法: DELETE
+    路径: /api/products/{id}
+    config: none
+ * @param id 
+ * @returns delete result
+ */
+export const deleteProduct = (id: string) => {
+    return axios.delete(`${API_PRODUCT_MODULE_PREFIX}/${id}`)
+    .then(res => {
+        return res
+    })
+}
+
+/**
+ * 请求方式
+    方法: PATCH
+    路径: /api/products/stockpile/{id}
+    config: application/json
+    * @param productinfo
+    * @returns update result
+ */
+export const updateStockpile = (productinfo : productAmount) => {
+    return axios.patch(`${API_PRODUCT_MODULE_PREFIX}/stockpile/${productinfo.id}`,productinfo,
+        {headers: { 'Content-Type': 'application/json' }})
+    .then(res => {
+        return res
+    })
+}
+
+/**
+ * 请求方式
+ *  方法: GET
+ *  路径: /api/products/stockpile/{id}
+ *  config: none
+ * @param id
+ * @returns product stockpile
+ * */
+export const getStockpile = (id: string) => {
+    return axios.get(`${API_PRODUCT_MODULE_PREFIX}/stockpile/${id}`)
     .then(res => {
         return res
     })
