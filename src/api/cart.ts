9ea515b666c5319a0c2d733b1843cc2f75ec0c17
Ethan-shen-Official <13382012505@163.com>
2025-04-12 21:37:26 +0800

    initialize cart and order api

---
+++ src/api/cart.ts
@@ -0,0 +1,4 @@
+import { API_CART_MODULE_PREFIX } from "./_prefix";
+import { axios } from "../utils/request";
+
+//TODO:
---
+++ src/api/order.ts
@@ -0,0 +1,4 @@
+import { API_ORDER_MODULE_PREFIX } from "./_prefix";
+import { axios } from "../utils/request";
+
+//TODO:
