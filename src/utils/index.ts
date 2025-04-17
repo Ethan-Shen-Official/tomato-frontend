8197ee8a2dc6b79322134df3ca13354ba4086009
Ethan-shen-Official <13382012505@163.com>
2025-04-18 00:21:23 +0800

    modify header

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,11 @@ export function parseRole(role: string | null) {
         return '会员';
     }
 }
+
+export function isLogin() {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+        return true;
+    }
+    return false;
+}
