57c196962dc1447234ee8adb9066280818de1320
Ethan-shen-Official <13382012505@163.com>
2025-06-22 00:22:34 +0800

    fix backend url

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import 'element-plus/dist/index.css'
 import './style.css'
 
 //BackEnd Port
-axios.defaults.baseURL = ("http://localhost:8080")
+axios.defaults.baseURL = ("http://47.116.112.229/:8080")
 axios.defaults.timeout = 30000;
 
 createApp(App).use(ElementPlus).use(routes).mount('#app')
